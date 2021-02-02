f4b31ebbf0ad271fec1fa073f926df8a024836c9
billysm64 <billysm64@gmail.com>
2021-02-02 14:38:12 -0500

    Add base HTML, token key to JS

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
-f
+
+
+(function() {
+
+  'use strict';
+
+  const GITHUB_TOKEN = "2e836bc26c41da74201f232506621ac16961303e";
+  const BASE_URL = 'https://api.github.com/'; //http://api.nasa.gov//
+
+  fetch(`${BASE_URL}`, {
+    headers: {
+      Authorization: `token ${GITHUB_TOKEN}`
+    }
+  })
+    .then(response => response.json())
+    .then(data => console.log(data));
