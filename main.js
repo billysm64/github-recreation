b41fbd0eb722430b08572900fc95387ea1b098cd
billysm64 <billysm64@gmail.com>
2021-02-03 08:37:51 -0500

    Submit broken code

--- main.js
+++ main.js
@@ -1,16 +1,20 @@
+const BASE_URL = 'https://api.github.com/users/billysm64'; //http://api.nasa.gov//
 
+console.log(GITHUB_TOKEN)
+fetch(`${BASE_URL}`, {
+  headers: {
+    Authorization: `token ${GITHUB_TOKEN}`
+  }
+})
+  .then(response => response.json())
+  .then(data => console.log(data));
 
-(function() {
+const repos = (data) => {
+  const source = document.getElementById("repositories").innerHTML;
+  const template = Handlebars.compile(source);
+  const context = data;
+  const html = template(context);
+  document.querySelector(".repositories").innerHTML = html;
+}
 
-  'use strict';
-
-  const GITHUB_TOKEN = "2e836bc26c41da74201f232506621ac16961303e";
-  const BASE_URL = 'https://api.github.com/'; //http://api.nasa.gov//
-
-  fetch(`${BASE_URL}`, {
-    headers: {
-      Authorization: `token ${GITHUB_TOKEN}`
-    }
-  })
-    .then(response => response.json())
-    .then(data => console.log(data));
+console.log(data.created_at)
